e5211550443975f82ca0017a59692440436f0868
rtman <ryanjtrann@gmail.com>
2019-12-24 00:10:41 -0800

    implemented yarn workspaces

--- packages/graphql/src/main.ts
+++ packages/graphql/src/main.ts
@@ -1,8 +1,8 @@
 import cors from 'cors';
 import express from 'express';
-import {ApolloServer} from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import 'dotenv/config';
-import models, {sequelize} from './models';
+import { sequelize } from './models';
 
 const app = express();
 
@@ -12,16 +12,16 @@ const server = new ApolloServer({});
 
 server.applyMiddleware({
   app,
-  path: '/graphql',
+  path: '/graphql'
 });
 
 sequelize.sync().then(async () => {
   app.listen(
     {
-      port: 8000, // you could change this to process.env.PORT if you wish
+      port: 8000 // you could change this to process.env.PORT if you wish
     },
     () => {
       console.log('Apollo Server on http://localhost:8000/graphql');
-    },
+    }
   );
 });
