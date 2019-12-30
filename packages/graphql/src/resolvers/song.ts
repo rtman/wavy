06becf2dcb10f228a043ddec4e84137da503effe
rtman <ryanjtrann@gmail.com>
2019-12-29 23:35:49 -0800

    mutations added

--- packages/graphql/src/resolvers/song.ts
+++ packages/graphql/src/resolvers/song.ts
@@ -5,48 +5,27 @@
 export default {
   Query: {
     songs: async (parent, args, { models }) => {
-      console.log('resolvers songs parent', parent);
-      console.log('resolvers songs args', args);
-      console.log('resolvers songs models', models);
       return await models.Song.findAll();
     },
     song: async (parent, { id }, { models }) => {
-      console.log('resolvers song parent', parent);
-      console.log('resolvers song id', id);
-      console.log('resolvers song models', models);
-
       return await models.Song.findByPk(id);
     }
+  },
+  Mutation: {
+    createNewSong: async (parent, { title, artist }, { models }) => {
+      return await models.Song.create({
+        title,
+        artist
+      });
+    },
+    updateSongTitle: async (parent, { id, title }, { models }) => {
+      const song = await models.Song.findByPk(id);
+      return await song.update({ title });
+    },
+    deleteSong: async (parent, { id }, { models }) => {
+      return await models.Song.destroy({
+        where: { id }
+      });
+    }
   }
-  // Mutation: {
-  //   createNewSong: async (parent, { text }, { models }) => {
-  //     return await models.Song.create({
-  //       text
-  //     });
-  //   },
-
-  //   deleteSong: async (parent, { id }, { models }) => {
-  //     return await models.Song.destroy({
-  //       where: {
-  //         id
-  //       }
-  //     });
-  //   },
-  //   updateSong: async (parent, { id, text }, { models }) => {
-  //     await models.Song.update(
-  //       {
-  //         text
-  //       },
-  //       {
-  //         where: {
-  //           id: id
-  //         }
-  //       }
-  //     );
-  //     const updatedSong = await models.Song.findByPk(id, {
-  //       include
-  //     });
-  //     return updatedSong;
-  //   }
-  // }
 };
--- packages/graphql/src/schema/song.ts
+++ packages/graphql/src/schema/song.ts
@@ -1,19 +1,22 @@
 import { gql } from 'apollo-server-express';
 
 export default gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Song" type defines the queryable fields for every song in our data source.
-  type Song {
-    title: String
-    artist: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   extend type Query {
     songs: [Song]
     song(id: ID!): Song
+  }
+
+  extend type Mutation {
+    createNewSong(title: String!, artist: String!): Song!
+    updateSongTitle(id: ID!, title: String!): Song!
+    deleteSong(id: ID!): Boolean!
+  }
+
+  type Song {
+    title: String
+    artist: String
+    id: ID
+    createdAt: Date
+    updatedAt: Date
   }
 `;
