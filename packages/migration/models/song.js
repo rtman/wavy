8cb56fb1d154286d33257be4ac33732284f9f1ad
rtman <ryanjtrann@gmail.com>
2020-01-05 18:58:46 -0800

    prettier

--- packages/migration/models/song.js
+++ packages/migration/models/song.js
@@ -1,9 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Song = sequelize.define('Song', {
-    title: DataTypes.STRING,
-    artist: DataTypes.STRING
-  }, {});
+  const Song = sequelize.define(
+    'Song',
+    {
+      title: DataTypes.STRING,
+      artist: DataTypes.STRING
+    },
+    {}
+  );
   Song.associate = function(models) {
     // associations can be defined here
   };
