ff26491665a50f8a8271c721e849e82edeb8582a
rtman <ryanjtrann@gmail.com>
2020-01-08 23:07:14 -0800

    setting up mobile

---
+++ packages/mobile/src/index.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react';
+import { View, Text } from 'react-native';
+
+export const App = (): ReactNode => {
+  return (
+    <>
+      <View>
+        <Text>"Test"</Text>
+      </View>
+    </>
+  );
+};
