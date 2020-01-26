d7da5928769675d3006e3c5e891758a82b89d3a8
rtman <ryanjtrann@gmail.com>
2020-01-25 18:51:01 -0800

    remove extend

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'jest', 'prettier'],
   extends: [
-    'react-app',
     '@react-native-community',
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
