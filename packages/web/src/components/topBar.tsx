224dd544d1a5a3f84afb76d2c6835dbfda20002d
rtman <ryanjtrann@gmail.com>
2020-06-16 14:03:56 -0700

    expanding search button

--- packages/web/src/components/topBar.tsx
+++ packages/web/src/components/topBar.tsx
@@ -1,266 +1,16 @@
-// import React from 'react';
-// import {
-//   fade,
-//   makeStyles,
-//   Theme,
-//   createStyles,
-// } from '@material-ui/core/styles';
-// import {
-//   AppBar,
-//   Badge,
-//   Toolbar,
-//   IconButton,
-//   Typography,
-//   InputBase,
-//   MenuItem,
-//   Menu,
-// } from '@material-ui/core';
-
-// import MenuIcon from '@material-ui/icons/Menu';
-// import SearchIcon from '@material-ui/icons/Search';
-// import AccountCircle from '@material-ui/icons/AccountCircle';
-// import MailIcon from '@material-ui/icons/Mail';
-// import NotificationsIcon from '@material-ui/icons/Notifications';
-// import MoreIcon from '@material-ui/icons/MoreVert';
-
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     grow: {
-//       flexGrow: 1,
-//     },
-//     menuButton: {
-//       marginRight: theme.spacing(2),
-//     },
-//     title: {
-//       display: 'none',
-//       [theme.breakpoints.up('sm')]: {
-//         display: 'block',
-//       },
-//     },
-//     search: {
-//       position: 'relative',
-//       borderRadius: theme.shape.borderRadius,
-//       backgroundColor: fade(theme.palette.common.white, 0.15),
-//       '&:hover': {
-//         backgroundColor: fade(theme.palette.common.white, 0.25),
-//       },
-//       marginRight: theme.spacing(2),
-//       marginLeft: 0,
-//       width: '100%',
-//       [theme.breakpoints.up('sm')]: {
-//         marginLeft: theme.spacing(3),
-//         width: 'auto',
-//       },
-//     },
-//     searchIcon: {
-//       width: theme.spacing(7),
-//       height: '100%',
-//       position: 'absolute',
-//       pointerEvents: 'none',
-//       display: 'flex',
-//       alignItems: 'center',
-//       justifyContent: 'center',
-//     },
-//     inputRoot: {
-//       color: 'inherit',
-//     },
-//     inputInput: {
-//       padding: theme.spacing(1, 1, 1, 7),
-//       transition: theme.transitions.create('width'),
-//       width: '100%',
-//       [theme.breakpoints.up('md')]: {
-//         width: 200,
-//       },
-//     },
-//     sectionDesktop: {
-//       display: 'none',
-//       [theme.breakpoints.up('md')]: {
-//         display: 'flex',
-//       },
-//     },
-//     sectionMobile: {
-//       display: 'flex',
-//       [theme.breakpoints.up('md')]: {
-//         display: 'none',
-//       },
-//     },
-//   })
-// );
-
-// export const TopBar = () => {
-//   const classes = useStyles();
-//   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-//   const [
-//     mobileMoreAnchorEl,
-//     setMobileMoreAnchorEl,
-//   ] = React.useState<null | HTMLElement>(null);
-
-//   const isMenuOpen = Boolean(anchorEl);
-//   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-//   const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleMobileMenuClose = () => {
-//     setMobileMoreAnchorEl(null);
-//   };
-
-//   const handleMenuClose = () => {
-//     setAnchorEl(null);
-//     handleMobileMenuClose();
-//   };
-
-//   const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-//     setMobileMoreAnchorEl(event.currentTarget);
-//   };
-
-//   const menuId = 'primary-search-account-menu';
-//   const renderMenu = (
-//     <Menu
-//       anchorEl={anchorEl}
-//       anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-//       id={menuId}
-//       keepMounted
-//       transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-//       open={isMenuOpen}
-//       onClose={handleMenuClose}
-//     >
-//       <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-//       <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-//     </Menu>
-//   );
-
-//   const mobileMenuId = 'primary-search-account-menu-mobile';
-//   const renderMobileMenu = (
-//     <Menu
-//       anchorEl={mobileMoreAnchorEl}
-//       anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-//       id={mobileMenuId}
-//       keepMounted
-//       transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-//       open={isMobileMenuOpen}
-//       onClose={handleMobileMenuClose}
-//     >
-//       <MenuItem>
-//         <IconButton aria-label="show 4 new mails" color="inherit">
-//           <Badge badgeContent={4} color="secondary">
-//             <MailIcon />
-//           </Badge>
-//         </IconButton>
-//         <p>Messages</p>
-//       </MenuItem>
-//       <MenuItem>
-//         <IconButton aria-label="show 11 new notifications" color="inherit">
-//           <Badge badgeContent={11} color="secondary">
-//             <NotificationsIcon />
-//           </Badge>
-//         </IconButton>
-//         <p>Notifications</p>
-//       </MenuItem>
-//       <MenuItem onClick={handleProfileMenuOpen}>
-//         <IconButton
-//           aria-label="account of current user"
-//           aria-controls="primary-search-account-menu"
-//           aria-haspopup="true"
-//           color="inherit"
-//         >
-//           <AccountCircle />
-//         </IconButton>
-//         <p>Profile</p>
-//       </MenuItem>
-//     </Menu>
-//   );
-
-//   return (
-//     <div className={classes.grow}>
-//       <AppBar position="static">
-//         <Toolbar>
-//           <IconButton
-//             edge="start"
-//             className={classes.menuButton}
-//             color="inherit"
-//             aria-label="open drawer"
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography className={classes.title} variant="h6" noWrap>
-//             Material-UI
-//           </Typography>
-//           <div className={classes.search}>
-//             <div className={classes.searchIcon}>
-//               <SearchIcon />
-//             </div>
-//             <InputBase
-//               placeholder="Search…"
-//               classes={{
-//                 root: classes.inputRoot,
-//                 input: classes.inputInput,
-//               }}
-//               inputProps={{ 'aria-label': 'search' }}
-//             />
-//           </div>
-//           <div className={classes.grow} />
-//           <div className={classes.sectionDesktop}>
-//             <IconButton aria-label="show 4 new mails" color="inherit">
-//               <Badge badgeContent={4} color="secondary">
-//                 <MailIcon />
-//               </Badge>
-//             </IconButton>
-//             <IconButton aria-label="show 17 new notifications" color="inherit">
-//               <Badge badgeContent={17} color="secondary">
-//                 <NotificationsIcon />
-//               </Badge>
-//             </IconButton>
-//             <IconButton
-//               edge="end"
-//               aria-label="account of current user"
-//               aria-controls={menuId}
-//               aria-haspopup="true"
-//               onClick={handleProfileMenuOpen}
-//               color="inherit"
-//             >
-//               <AccountCircle />
-//             </IconButton>
-//           </div>
-//           <div className={classes.sectionMobile}>
-//             <IconButton
-//               aria-label="show more"
-//               aria-controls={mobileMenuId}
-//               aria-haspopup="true"
-//               onClick={handleMobileMenuOpen}
-//               color="inherit"
-//             >
-//               <MoreIcon />
-//             </IconButton>
-//           </div>
-//         </Toolbar>
-//       </AppBar>
-//       {renderMobileMenu}
-//       {renderMenu}
-//     </div>
-//   );
-// };
-
 import React, { FunctionComponent } from 'react';
 import { MenuDrawer } from 'components';
 import {
   AppBar,
   CssBaseline,
-  // Divider,
   Drawer,
   Hidden,
-  // ListItem,
-  // ListItemIcon,
-  // ListItemText,
   IconButton,
   InputBase,
   Toolbar,
   Typography,
 } from '@material-ui/core';
-// import InboxIcon from '@material-ui/icons/MoveToInbox';
-// import List from '@material-ui/core/List';
-// import MailIcon from '@material-ui/icons/Mail';
+
 import SearchIcon from '@material-ui/icons/Search';
 import MenuIcon from '@material-ui/icons/Menu';
 import {
@@ -334,11 +84,16 @@ const useStyles = makeStyles((theme: Theme) =>
       color: 'inherit',
     },
     inputInput: {
-      padding: theme.spacing(1, 1, 1, 7),
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
       transition: theme.transitions.create('width'),
       width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: 200,
+      [theme.breakpoints.up('sm')]: {
+        width: '0ch',
+        '&:focus': {
+          width: '20ch',
+        },
       },
     },
   })
