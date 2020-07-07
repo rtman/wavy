80421bdf3fcde4b4fd66a0fe2069bc5069d586c9
rtman <ryanjtrann@gmail.com>
2020-07-07 11:50:22 -0700

    refactored song input with react hooks form

--- packages/web/src/screens/createRelease.tsx
+++ packages/web/src/screens/createRelease.tsx
@@ -19,11 +19,19 @@ import * as consts from 'consts';
 // import { UserContext } from 'context';
 import React, {
   // useContext,
+  memo,
   useState,
 } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray, Controller } from 'react-hook-form';
 // import { useHistory, useLocation, useParams } from 'react-router-dom';
 // import { Album } from 'types';
+
+interface SongInput {
+  title: string;
+  // genres: string[];
+  // releaseDate: Date;
+  // supportingArtists: string[];
+}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -45,15 +53,51 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// const SongInputRow = ({ index }: { index: number }) => {
+//   const { getValues, setValue } = useFormContext();
+//   const values = getValues();
+//   const titleKey = `[${index}].title`;
+//   const title = values[titleKey];
+
+//   return (
+//     <TextField
+//       defaultValue={title}
+//       variant="outlined"
+//       margin="normal"
+//       required
+//       fullWidth
+//       id={titleKey}
+//       label="Title"
+//       name={titleKey}
+//       autoComplete="title"
+//       onChange={(e) => {
+//         setValue(titleKey, e.target.value);
+//       }}
+//     />
+//   );
+// };
+
 export const CreateRelease = () => {
   //   const userContext = useContext(UserContext);
   //   const history = useHistory();
   //   const location = useLocation();
   //   const { id } = useParams();
   const classes = useStyles();
-  const { register, handleSubmit } = useForm();
 
   const [numberOfSongs, setNumberOfSongs] = useState<number>(1);
+  const [inputList, setInputList] = useState<SongInput[]>([{ title: '' }]);
+
+  const { register, control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      songs: [{ title: '' }],
+    },
+  });
+  const { fields, append, prepend, remove, swap, move, insert } = useFieldArray(
+    {
+      control,
+      name: 'songs',
+    }
+  );
   //   const [artist, setArtist] = useState<ArtistType | undefined>(undefined);
 
   //   const [createAlbum] = useMutation(consts.mutations.CREATE_PLAYLIST, {
@@ -74,66 +118,45 @@ export const CreateRelease = () => {
   //     },
   //   });
 
-  const onSubmit = (data: Record<string, any>) => {
-    console.log('Data', data);
-  };
+  // const renderSongInputForm = () => {
+  //   const songInputsList: JSX.Element[] = [];
 
-  const renderSongInputForm = () => {
-    const songInputsList: JSX.Element[] = [];
+  //   const makeSongInputs = () => {
+  //     for (let i = 0; i < numberOfSongs; i++) {
+  //       songInputsList.push(
+  //         <React.Fragment key={`${numberOfSongs}-song-input`}>
+  //           {SongInputRow({ index: i })}
+  //         </React.Fragment>
+  //       );
+  //     }
+  //     return <List>{songInputsList}</List>;
+  //   };
 
-    const makeSongInputs = () => {
-      for (let i = 0; i < numberOfSongs; i++) {
-        songInputsList.push(
-          <React.Fragment key={`group-${numberOfSongs}`}>
-            <TextField
-              inputRef={register}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id={`email-${i}`}
-              label="Email Address"
-              name={`email-${i}`}
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              inputRef={register}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name={`password-${i}`}
-              label="Password"
-              type="password"
-              id={`password-${i}`}
-              autoComplete="current-password"
-            />
-          </React.Fragment>
-        );
-      }
-      return <List>{songInputsList}</List>;
-    };
+  //   const onSubmit = (data: Record<string, any>) => {
+  //     console.log('Data', data);
+  //   };
 
-    return (
-      <form
-        className={classes.form}
-        noValidate
-        onSubmit={handleSubmit((data) => onSubmit(data))}
-      >
-        {makeSongInputs()}
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-        >
-          Submit
-        </Button>
-      </form>
-    );
-  };
+  //   return (
+  //     <form
+  //       className={classes.form}
+  //       noValidate
+  //       onSubmit={handleSubmit((data) => onSubmit(data))}
+  //     >
+  //       {makeSongInputs()}
+  //       <Button
+  //         type="submit"
+  //         fullWidth
+  //         variant="contained"
+  //         color="primary"
+  //         className={classes.submit}
+  //       >
+  //         Submit
+  //       </Button>
+  //     </form>
+  //   );
+  // };
+
+  const onSubmit = (data: any) => console.log('data', data);
 
   return (
     <Container>
@@ -143,17 +166,76 @@ export const CreateRelease = () => {
 
         <Spacing.section.Minor />
 
-        <IconButton
-          color="primary"
-          onClick={() => setNumberOfSongs(numberOfSongs + 1)}
-        >
-          <MoreIcon />
-          Add Another
-        </IconButton>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <List>
+            {fields.map((item, index) => {
+              return (
+                <React.Fragment key={item.id}>
+                  {/* <TextField
+                    inputRef={register()}
+                    defaultValue={`${item.title}`}
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name={`[${index}].title`}
+                    label="Title"
+                    id={`[${index}].title`}
+                    autoComplete="title"
+                  /> */}
 
-        {renderSongInputForm()}
+                  <Controller
+                    as={<TextField />}
+                    // inputRef={register()}
+                    variant="outlined"
+                    margin="normal"
+                    required={true}
+                    fullWidth={true}
+                    name={`songs[${index}].title`}
+                    label="Title"
+                    id={`songs[${index}].title`}
+                    autoComplete="title"
+                    control={control}
+                    defaultValue={item.title} // make sure to set up defaultValue
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    onClick={() => remove(index)}
+                  >
+                    Delete
+                  </Button>
+                </React.Fragment>
+              );
+            })}
+          </List>
 
-        <Spacing.section.Major />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={() => {
+              append({ title: 'appendBill' });
+            }}
+          >
+            Add Song
+          </Button>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+        </form>
       </Flex>
     </Container>
   );
