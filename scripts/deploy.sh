3b9c39cbfb21a3be807e05bd0e15698ed5f988b4
rtman <ryanjtrann@gmail.com>
2020-07-12 12:45:58 -0700

    add web dev env to docker compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 gcloud container clusters create groov --num-nodes=1
 gcloud container clusters get-credentials groov
-kubectl apply -f ./backend.yml 
-kubectl apply -f ./frontend.yml 
+kubectl apply -f ./docker/backend.yml 
+kubectl apply -f ./docker/frontend.yml 
