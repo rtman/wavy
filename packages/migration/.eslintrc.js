60af95e90554a59194e4cdc60ae248cb51d7c0c7
rtman <ryanjtrann@gmail.com>
2020-07-27 12:12:07 -0700

    update migration eslint

--- packages/migration/.eslintrc.js
+++ packages/migration/.eslintrc.js
@@ -2,6 +2,12 @@ const baseConfig = require('../../.eslintrc.js');
 
 module.exports = {
   ...baseConfig,
+  env: {
+    node: true,
+    // browser: true,
+    // commonjs: true,
+    // es6: true,
+  },
   rules: {
     ...baseConfig.rules,
     camelcase: ['error', { properties: 'never' }],
