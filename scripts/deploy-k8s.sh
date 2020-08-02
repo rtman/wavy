8de81f139c13b721464af265b3b9b90b6130d766
rtman <ryanjtrann@gmail.com>
2020-08-02 14:23:27 -0700

    deploy script working

--- scripts/deploy-k8s.sh
+++ scripts/deploy-k8s.sh
@@ -1,14 +1,42 @@
 #!/bin/sh
+set -euxo pipefail
 
-./createClusters.sh
-./build-backend-images.sh
-./push-backend-images.sh
-./apply-backend.sh
+echo "Creating Cluster"
+./scripts/createClusters.sh
 
-EXTERNAL_IP=`kubectl get svc groov-backend-public --output jsonpath='{.status.loadBalancer.ingress[0].ip'`
+echo "Building backend"
+./scripts/build-backend-images.sh
+./scripts/push-backend-images.sh
+./scripts/apply-backend.sh
 
-sed -i -e "s/.*REACT_APP_GRAPHQL_IP.*/REACT_APP_GRAPHQL_IP=$EXTERNAL_IP/" ../packages/web/.env
+echo "Waiting for backend public external IP"
+while true; do
 
-./build-frontend-images.sh
-./push-frontend-images.sh
-./apply-frontend.sh
+    STATUS=$(kubectl get pods -o jsonpath={'.items[0].status.phase'})
+    if [[ $STATUS == 'Running' ]]; 
+    then 
+        echo "external IP available, Done waiting"
+        break
+    fi
+
+    echo "external IP not available, waiting 30 seconds"
+
+    sleep 30
+done
+
+echo "Getting backend external ip"
+EXTERNAL_IP=$(kubectl get svc groov-backend-public --output jsonpath='{.status.loadBalancer.ingress[0].ip}') 
+
+echo "groov-backend-public external ip = $EXTERNAL_IP"
+
+echo "Updating REACT_APP_GRAPHQL_IP env variable with current external ip"
+
+sed -i -e "s/.*REACT_APP_GRAPHQL_IP.*/REACT_APP_GRAPHQL_IP=$EXTERNAL_IP/" ./packages/web/.env
+
+echo "Building frontend"
+
+./scripts/build-frontend-images.sh
+./scripts/push-frontend-images.sh
+./scripts/apply-frontend.sh
+
+echo "Done!"
