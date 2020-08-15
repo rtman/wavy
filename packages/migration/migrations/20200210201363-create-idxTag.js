db68c7421e73c7cfccef2424a5b51deb87f1a3c3
rtman <ryanjtrann@gmail.com>
2020-08-15 11:06:40 -0700

    fix tag index

--- packages/migration/migrations/20200210201363-create-idxTag.js
+++ packages/migration/migrations/20200210201363-create-idxTag.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: (queryInterface) => {
     return queryInterface.sequelize.query(
-      `CREATE INDEX idxTag ON playlist USING zombodb ((tag.*)) WITH (url="${process.env.ELASTICSEARCH_HOST}:9200/");`
+      `CREATE INDEX idxTag ON tag USING zombodb ((tag.*)) WITH (url="${process.env.ELASTICSEARCH_HOST}:9200/");`
     );
   },
   down: (queryInterface) => {
